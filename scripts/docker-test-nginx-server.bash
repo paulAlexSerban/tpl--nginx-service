b1b3a9cc986cbab315e7046434958d2f8f421312
paulAlexSerban <paul.alex.serban@gmail.com>
2022-07-13 19:53:24 +0300

    refactor(structure): refactor structure

--- scripts/docker-test-nginx-server.bash
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-cd $(dirname $0) # makes sure the folder containing the script will be the root folder
-echo "🧪  TEST Nginx server"
-bash ../docker/test/check-docker-container.bash nginx
