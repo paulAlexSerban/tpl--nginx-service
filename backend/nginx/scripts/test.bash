f90eab3e56bd0c1aaddf9974964e8738701b060b
paulAlexSerban <paul.alex.serban@gmail.com>
2023-05-29 17:03:29 +0300

    revert: initial refactor

--- backend/nginx/scripts/test.bash
+++ backend/nginx/scripts/test.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 # makes sure the folder containing the script will be the root folder
 cd "$(dirname "$0")" || exit
-echo "🔍  Lint Frontend Nginx"
-npm --prefix ../frontend/nginx run lint
+echo "🧪  TEST Nginx server"
+bash ../../scripts/test/check-docker-container.bash nginx
---
+++ frontend/nginx/scripts/watch.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
+
+echo "Watching Frontend Nginx "
+npm --prefix .. run watch
